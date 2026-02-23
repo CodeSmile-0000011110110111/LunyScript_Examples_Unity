1d4165135d248f2d37eb9d771b683c89402d6c2f
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-23 13:48:55 +0100

    Sistato: create playground scene, player moves, camera follows

---
+++ Assets/Sistato/LunyScripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using LunyScript;
+using System;
+
+namespace Sistato.LunyScripts
+{
+	public class Player : Script
+	{
+		public override void Build(ScriptContext context)
+		{
+			const Single Speed = 15f;
+
+			On.Ready(Debug.Log($"Hello, {nameof(Player)}"));
+			On.FrameUpdate(Transform.MoveBy(Input.Direction("Move"), Speed));
+		}
+	}
+}
