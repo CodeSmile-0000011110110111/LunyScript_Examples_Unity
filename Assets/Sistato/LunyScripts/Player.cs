b6fe622b166c8e0ad61ce677b4bdbdb3f288d084
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-27 14:34:48 +0100

    refactor builders

--- Assets/Sistato/LunyScripts/Player.cs
+++ Assets/Sistato/LunyScripts/Player.cs
@@ -17,6 +17,11 @@ namespace Sistato.LunyScripts
 
 			On.Collision.Layered("Enemy").Cooldown(1).Begins(Debug.Log(">>>>> OUCH !!! <<<<<<"));
 
+			//For(3).Do(Debug.Log("for 3"));
+			//Coroutine("hello").For(1).Seconds().Do(Debug.Log("for a second"));
+			Every(10).Heartbeats().Do(Debug.Log("bu-bumm"));
+			Every(60).Heartbeats().Do(Debug.Log("60 frames passed"));
+
 			/*
 			On.Collision
 				.Layered("Enemy", "Enemy")
