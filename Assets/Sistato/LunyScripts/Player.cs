0369eac8383fcfa77bce19de542bdc1000b48c11
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-28 14:30:58 +0100

    coroutine For improvements/fixes

--- Assets/Sistato/LunyScripts/Player.cs
+++ Assets/Sistato/LunyScripts/Player.cs
@@ -19,37 +19,6 @@ namespace Sistato.LunyScripts
 			Run(MyActionMethod());
 			Run(MyVoidMethod);
 
-			// Counts frames or heartbeats: In() => once-only, Every() => repeating
-			// Counter("counter in beats").In(60).Heartbeats().Do(Debug.Log("counter in beats"));
-			// Counter("counter every beats").Every(60).Heartbeats().Do(Debug.Log("counter every beats"));
-			// Counter("counter in frames").In(60).Frames().Do(Debug.Log("counter in frames"));
-			// Counter("counter every frames").Every(60).Frames().Do(Debug.Log("counter every frames"));
-
-			// Counter("sdf").Every(3).Frames().Do();
-			// Every(3).Frames().Do();
-
-			// Every(2).Heartbeats().Do(Debug.Log("every even beat"));
-			// Every(2).Heartbeats().Offset(1).Do(Debug.Log("every odd beat"));
-			// Every(2).Frames().Do(Debug.Log("every even frame"));
-			// Every(2).Frames().Offset(1).Do(Debug.Log("every odd frame"));
-
-
-			var forRoutine = Coroutine("for")
-				.For(30)
-				.Frames()
-				.WhenStarted(Debug.Log("for STARTED"))
-				.WhenPaused(Debug.Log("for PAUSED"))
-				.WhenResumed(Debug.Log("for RESUMED"))
-				.WhenStopped(Debug.Log("for STOPPED"))
-				.WhenElapsed(Debug.Log("for ELAPSED"));
-
-
-			// Counter("for pause").Every(8).Frames().Do(forRoutine.Pause());
-			// Counter("for resume").Every(10).Frames().Do(forRoutine.Resume());
-			// Counter("for stop").In(12).Frames().Do(forRoutine.Stop());
-			// Counter("for start").In(15).Frames().Do(forRoutine.Start());
-
-
 			/*
 			On.Collision
 				.Layered("Enemy", "Enemy")
