0ea66c9fbd49948b9068e211500167fd31037082
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-03-04 22:06:35 +0100

    improve input handling, add input smoke test

--- Assets/Sistato/LunyScripts/Player.cs
+++ Assets/Sistato/LunyScripts/Player.cs
@@ -9,25 +9,25 @@ namespace Sistato.LunyScripts
 		{
 			const Single Speed = 9f;
 
-			When.InputAction("Move").Performing(Transform.ShiftBy(Input.Direction("Move"), Speed));
-			When.InputAction("Look").Performing(Transform.SetLocalRotation(Input.Rotation("Look")));
+			When.InputAction("Move")
+				.Begins(Debug.Log("Move begins ..."))
+				.Changes(Debug.Log("Move value changes ..."))
+				.Continues(Transform.ShiftBy(Input.Direction("Move"), Speed))
+				.Ends(Debug.Log("Move ends ..."));
+			When.InputAction("Look").Continues(Transform.SetLocalRotation(Input.Rotation("Look")));
 
 			var cameraMode = Var["CameraMode"];
 			cameraMode.Set(0);
 
 			var followCameraName = "CineCam (Flat Iso Player Follow)";
 			When.InputAction("ToggleCamera")
-				.Performed(If(cameraMode != 1)
+				.Changes(If(cameraMode != 1)
 					.Then(Debug.Log("ENTER topdown"), cameraMode.Inc(), Object.Disable(followCameraName))
 					.Else(Debug.Log("EXIT topdown"), cameraMode.Dec(), Object.Enable(followCameraName)));
 
 			On.Ready(Var["count"].Set(0),
 				For(10).Do(Debug.Log("for ...")),
 				While(Var["count"] < 10).Do(Var["count"].Inc(), Debug.Log("while ..."), Debug.Log(Var["count"])));
-
-			//InputAction("Move").WhenStarted().WhenPerformed().WhenCanceled().WhilePerforming();
-			//When.InputAction("Move").Started().Performed().Canceled();
-			//While.InputAction("Move").Performing();
 
 			/*
 			On.FrameUpdate(
